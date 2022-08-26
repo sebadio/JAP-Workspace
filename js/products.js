786597132c15a18d73ce4550914ad07659375c6b
Shivu-a <sebaingame@gmail.com>
2022-08-25 21:57:43 -0300

    Agregada funcionalidad a los botones de filtrado

--- js/products.js
+++ js/products.js
@@ -1,12 +1,4 @@
-// Esperamos a que se carguen los elementos del DOM:
-
-window.onload = async () => {
-  setUser();
-
-  // Agarramos el div en el que se van a encontrar los productos:
-  const container = document.getElementById("container");
-  const titulo = document.getElementById("title");
-
+const fetchProducts = async () => {
   // Hacemos el fetch de los productos y extraemos los productos
   const url = `https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem(
     "catID"
@@ -16,6 +8,61 @@ window.onload = async () => {
   const datos = await respuesta.json();
 
   const { products } = datos;
+
+  return { datos, products };
+};
+
+const poblarLista = (products) => {
+  // Agarramos la lista
+  const lista = document.getElementById("lista");
+
+  // Limpiamos la lista por si tiene datos antiguos
+  lista.innerHTML = "";
+
+  // Poblamos la lista con los productos y sus respectivas clases de Bootstrap
+  products.map((producto) => {
+    lista.innerHTML += `
+      <li class="list-group-item w-100 d-flex flex-row p-2 justify-content-between list-group-item-action cursor-active">
+          <div class="w-25 card p-1">
+              <img class="img-fluid" src="${producto.image}" >
+          </div>
+          <div class="flex-fill ms-3" >
+              <h2>${producto.name} - ${producto.currency}${producto.cost}</h2>
+              <p>${producto.description}</p>
+          </div>
+          <div>
+              <small>${producto.soldCount} Vendidos</small>
+          </div>
+      </li>`;
+  });
+};
+
+let descCount = false;
+
+const sorting = (array, tipo, desc) => {
+  array.sort((a, b) => {
+    if (a[tipo] < b[tipo]) return -1;
+    if (a[tipo] > b[tipo]) return 1;
+    return 0;
+  });
+
+  if (desc) array.reverse();
+
+  return array;
+};
+
+// Esperamos a que se carguen los elementos del DOM:
+
+window.onload = async () => {
+  setUser();
+
+  // Agarramos el div en el que se van a encontrar los productos:
+  const container = document.getElementById("container");
+  const titulo = document.getElementById("title");
+
+  const { datos, products } = await fetchProducts();
+
+  let initialArray = [...products];
 
   // Modificamos el titulo para que muestre lo que queremos:
 
@@ -27,30 +74,40 @@ window.onload = async () => {
   // Modificamos el contenido del div para preparalo para los datos que vamos a recibir
   container.innerHTML = `<ul id="lista" class="list-group w-100"></ul>`;
 
-  // Agarramos la lista
-  const lista = document.getElementById("lista");
+  poblarLista(products);
 
-  // Poblamos la lista con los productos y sus respectivas clases de Bootstrap
-  products.map((producto) => {
-    lista.innerHTML += `
-    <li class="list-group-item w-100 d-flex flex-row p-2 justify-content-between">
-        <div class="w-25 card p-1">
-            <img class="img-fluid" src="${producto.image}" >
-        </div>
-        <div class="flex-fill ms-3" >
-            <h2>${producto.name} - ${producto.currency}${producto.cost}</h2>
-            <p>${producto.description}</p>
-        </div>
-        <div>
-            <small>${producto.soldCount} Vendidos</small>
-        </div>
-    </li>`;
+  document.getElementById("sortAsc").addEventListener("click", () => {
+    let array = sorting(products, "name", false);
+    poblarLista(array);
   });
 
-  // A cada elemento lista le agregamos un eventListener que nos de los datos del mismo para despues redireccinarnos a la pagina correspondiente
-  Array.from(document.getElementsByTagName("li")).forEach((element) => {
-    element.addEventListener("click", (e) => {
-      console.log(e);
-    });
+  document.getElementById("sortDesc").addEventListener("click", () => {
+    let array = sorting(products, "name", true);
+    poblarLista(array);
+  });
+
+  document.getElementById("sortByCount").addEventListener("click", () => {
+    let array = sorting(products, "soldCount", descCount);
+    descCount = !descCount;
+    poblarLista(array);
+  });
+
+  document.getElementById("rangeFilterCount").addEventListener("click", () => {
+    let min = document.getElementById("rangeFilterCountMin");
+    let max = document.getElementById("rangeFilterCountMax");
+
+    if (min && min !== "" && max && max !== "") {
+      let array = products.filter(
+        (item) => item.soldCount > min.value && item.soldCount < max.value
+      );
+      poblarLista(array);
+
+      min.value = "";
+      max.value = "";
+    }
+  });
+
+  document.getElementById("clearRangeFilter").addEventListener("click", () => {
+    poblarLista(initialArray);
   });
 };
