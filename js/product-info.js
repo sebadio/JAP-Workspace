a56affe9565c7fb39d179de44345124aebb21d8b
Shivu-a <sebaingame@gmail.com>
2022-09-16 21:31:37 -0300

    arreglada la funcion que consigue la fecha

--- js/product-info.js
+++ js/product-info.js
@@ -99,12 +99,12 @@ const getDate = () => {
   const year = date.getFullYear();
   const month =
     date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-  const day = date.getDate < 10 ? `0${date.getDate()}` : date.getDate();
-  const hour = date.getHours < 10 ? `0${date.getHours()}` : date.getHours();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minutes =
-    date.getMinutes < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
   const seconds =
-    date.getSeconds < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
 
   return `${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
 };
